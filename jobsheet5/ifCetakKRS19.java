28f9191ee30dabb773558ad224b516f7ac20af45
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-09-30 13:53:42 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS19.java
+++ jobsheet5/ifCetakKRS19.java
@@ -4,15 +4,20 @@ public class ifCetakKRS19 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.print("Apakah UKT sudah lunas? (true/false)");
+        System.out.print("Apakah UKT sudah lunas? (true/false) : ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        // } else {
+        //     System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        // }
+
+        String pesan = (uktLunas)   ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" 
+                                    : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
+
         sc.close();
     }
 }

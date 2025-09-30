74a43e76fb4e7edec00b3ac3385853afdeb51eab
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-09-30 16:30:17 +0700

    Tugas 3 (menghapus bagian and)

--- jobsheet5/Latihan1.java
+++ jobsheet5/Latihan1.java
@@ -9,22 +9,12 @@ public class Latihan1 {
         System.out.print("Apakah anda sudah registrasi secara online? (true/false) : ");
         Boolean regis = input.nextBoolean();
         
-        // Sesuai syarat pertama (or)
         if (kartu || regis) {
             System.out.println("\nselamat anda telah mendapatkan izin memasuki perpustakaan kampus\nyaayyy!!");
         } else {
             System.out.println("\nmaaf anda tidak memenuhi syarat masuk perpustakaan kampus!" +
             "\nsilahkan membawa kartu mahasiswa atau melakukan registrasi secara online");
         }
-        
-        // Sesuai syarat kedua (and)
-        // if (kartu && regis) {
-        //     System.out.println("\nselamat anda telah mendapatkan izin memasuki perpustakaan kampus\nyaayyy!!");
-        // } else {
-        //     System.out.println("\nmaaf anda tidak memenuhi syarat masuk perpustakaan kampus!" +
-        //     "\nsilahkan membawa kartu mahasiswa dan melakukan registrasi secara online");
-        // }
-
         input.close();
     }
 }

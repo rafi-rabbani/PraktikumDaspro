447b4b102c1eea713578347793f75ed328324598
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-10-22 02:38:20 +0700

    Modifikasi Percobaan 1

--- jobsheet7/SiakadFor19.java
+++ jobsheet7/SiakadFor19.java
@@ -5,6 +5,9 @@ public class SiakadFor19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        String status;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
+
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -17,10 +20,22 @@ public class SiakadFor19 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                status = "Lulus";
+                jumlahLulus++;
+            } else {
+                status = "Tidak Lulus";
+                jumlahTidakLulus++;
+            }
+
+            System.out.println("Status mahasiswa ke-" + i + ": " + status + "\n");
         }
 
-        System.out.println("Nilai tertinggi: " + tertinggi);
-        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Nilai tertinggi\t\t\t: " + tertinggi);
+        System.out.println("Nilai terendah\t\t\t: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus\t\t: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus\t: " + jumlahTidakLulus);
         sc.close();
     }
 }

1894e3942d5ed4c18603cb3b1530ab278e7d5ea6
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-10 21:56:28 +0700

    Modifikasi Percobaan 1

--- jobsheet9/ArrayBilangan19.java
+++ jobsheet9/ArrayBilangan19.java
@@ -1,15 +1,10 @@
 package jobsheet9;
 public class ArrayBilangan19 {
     public static void main(String[] args) {
-        int[] bill = new int[4];
-        bill[0] = 5;
-        bill[1] = 13;
-        bill[2] = -7;
-        bill[3] = 17;
+        int[] bill = {5, 13, -7, 17};
 
-        System.out.println(bill[0]);
-        System.out.println(bill[1]);
-        System.out.println(bill[2]);
-        System.out.println(bill[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bill[i]);
+        }
     }
 }

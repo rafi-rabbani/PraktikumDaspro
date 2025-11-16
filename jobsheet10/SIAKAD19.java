9e459ff77a1dc1f51b7cd5c33a87c930061aceae
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-16 09:51:29 +0700

    Modifikasi Percobaan 4

--- jobsheet10/SIAKAD19.java
+++ jobsheet10/SIAKAD19.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class SIAKAD19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int mahasiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int mataKuliah = sc.nextInt();
+
+        int[][] nilai = new int[mahasiswa][mataKuliah];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
@@ -17,20 +22,20 @@ public class SIAKAD19 {
                 totalPerSiswa += nilai[i][j];
             }
 
-            System.out.println("Nilai rata-rata: " + (totalPerSiswa / 3));
+            System.out.println("Nilai rata-rata: " + (totalPerSiswa / mataKuliah));
         }
 
         System.out.println("\n===================================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < mataKuliah; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < mahasiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
 
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + (totalPerMatkul / 4));
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + (totalPerMatkul / mahasiswa));
         }
         sc.close();
     }

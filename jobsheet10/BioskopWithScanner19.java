36ae9e3cdb06af8a3d56c1c529150eedb153a3b8
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-16 09:50:44 +0700

    Modifikasi Percobaan 2

--- jobsheet10/BioskopWithScanner19.java
+++ jobsheet10/BioskopWithScanner19.java
@@ -1,5 +1,6 @@
 package jobsheet10;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BioskopWithScanner19 {
@@ -8,23 +9,83 @@ public class BioskopWithScanner19 {
         int baris, kolom;
         String nama, next;
         String[][] penonton = new String[4][2];
+        for (String[] strings : penonton) {
+            Arrays.fill(strings, "***");
+        }
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
-            sc.nextLine();
+        boolean i = true;
+        while (i) {
+            System.out.print(
+            "\nMenu yang Tersedia:" + 
+            "\n1. Input Penonton" +
+            "\n2. Tampilkan daftar penonton" +
+            "\n3. Exit" +
+            "\nSilahkan pilih menu (1-3): "
+            );
+            String menu = sc.nextLine();
 
-            penonton[baris - 1][kolom - 1] = nama;
+            switch (menu) {
+                case "1":
+                    while (true) {
+                        System.out.print("\nMasukkan nama: ");
+                        nama = sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                        while (true) {
+                            while (true) {
+                                System.out.print("Masukkan baris: ");
+                                baris = sc.nextInt();
+    
+                                if (baris < 1 || baris > 4) {
+                                    System.out.println("Baris tidak tersedia. Masukkan nilai antara 1 hingga 4.");
+                                } else {
+                                    break;
+                                }
+                            }
+    
+                            while (true) {
+                                System.out.print("Masukkan kolom: ");
+                                kolom = sc.nextInt();
+    
+                                if (kolom < 1 || kolom > 2) {
+                                    System.out.println("Kolom tidak tersedia. Masukkan nilai antara 1 hingga 2.");
+                                } else {
+                                    break;
+                                }
+                            }
+                            sc.nextLine();
+                
+                            if (penonton[baris - 1][kolom - 1] != "***") {
+                                System.out.println("Kursi yang dipilih sudah terisi! Mohon pilih kursi lain!");
+                            } else {
+                                penonton[baris - 1][kolom - 1] = nama;
+                                break;
+                            }
+                        }
+            
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                        next = sc.nextLine();
+            
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                case "2":
+                    System.out.println();
+                    for (int j = 0; j < penonton.length; j++) {
+                        System.out.println("Penonton pada baris ke-" + (j + 1) + ": " + String.join(", ", penonton[j]));
+                    }
+                    break;
+
+                case "3":
+                    System.out.println("\nkeluar dari program.");
+                    i = false;
+                    break;
+
+                default:
+                    System.out.println("\nMenu tidak valid. Silahkan pilih antara 1-3.");
+                    break;
             }
         }
         sc.close();

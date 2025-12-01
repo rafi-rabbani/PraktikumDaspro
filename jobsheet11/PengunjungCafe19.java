5865a867515dd5d50121cdc8299c4fa4067514b3
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-12-01 19:12:53 +0700

    Percobaan 4

---
+++ jobsheet11/PengunjungCafe19.java
@@ -0,0 +1,16 @@
+package jobsheet11;
+
+public class PengunjungCafe19 {
+    static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}

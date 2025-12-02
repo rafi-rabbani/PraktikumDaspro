a112d9d2ea587b615c2943f4d2552e81041cf301
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-12-02 16:09:29 +0700

    Case Method 2: method menu

--- jobsheet13/Prestasi.java
+++ jobsheet13/Prestasi.java
@@ -8,6 +8,26 @@ public class Prestasi {
     static String[][] prestasi = new String[100][5];
     static int jumlahPrestasi = 0;
 
+    static int menu() {
+            System.out.print(
+                "\nMenu Prestasi Mahasiswa:\n" +
+                "1. Tambah Data Prestasi\n" +
+                "2. Tampilkan Semua Data Prestasi\n" +
+                "3. Mencari Data Prestasi Berdasarkan Tahun\n" +
+                "4. Hitung Jumlah Prestasi per Tingkat\n" +
+                "5. Keluar\n" +
+                "Pilih menu (1-5): "
+            );
+            int pilihan = input.nextInt();
+            input.nextLine();
+
+            while (pilihan < 1 || pilihan > 5) {
+                System.out.print("\nPilihan tidak valid\nPilih menu (1-5): ");
+                pilihan = input.nextInt();
+                input.nextLine();
+            }
+            return pilihan;
+    }
     public static void main(String[] args) {
         System.out.println("\n================= PENCATATAN PRESTASI MAHASISWA ================");
 
